07485e0e18e60f9764ed47fb794128eb7e538c92
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-05-18 15:55:29 -0500

    nice

--- components/pricing/Pricing.tsx
+++ components/pricing/Pricing.tsx
@@ -1,35 +1,48 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "./pricing.module.css"
 import { pricingFeatures, pricingTable, pricingTables } from './pricingTypes'
 import { formatter } from '@/utility/globalState'
 import FourthButton from '../fourthButton/FourthButton'
 
 export default function Pricing() {
+    const containerRef = useRef<HTMLDivElement>(null!)
+
+    function scrollToRecommended(currentPosition: number) {
+        containerRef.current.scrollLeft = currentPosition
+    }
 
     return (
-        <div className='snap' style={{ display: "grid", gap: "1rem", gridAutoFlow: "column", gridAutoColumns: "300px", overflowX: "auto", marginBlock: "1rem", padding: "1rem" }}>
+        <div ref={containerRef} className='snap' style={{ display: "grid", gap: "1rem", gridAutoFlow: "column", gridAutoColumns: "300px", overflowX: "auto", marginBlock: "1rem", padding: "1rem", scrollBehavior: "smooth" }}>
             {pricingTables.map((eachTable, eachTableIndex) => {
                 return (
-                    <DisplayPricingTable key={eachTableIndex} pricingTable={eachTable} startHovering={eachTableIndex === 2} recommended={eachTableIndex === 2} />
+                    <DisplayPricingTable key={eachTableIndex} pricingTable={eachTable} startHovering={eachTableIndex === 2} recommended={eachTableIndex === 2} scrollToRecommended={scrollToRecommended} />
                 )
             })}
         </div>
     )
 }
 
-function DisplayPricingTable({ pricingTable, startHovering, recommended }: { pricingTable: pricingTable, startHovering?: boolean, recommended?: boolean }) {
+function DisplayPricingTable({ pricingTable, startHovering, recommended, scrollToRecommended }: { pricingTable: pricingTable, startHovering?: boolean, recommended?: boolean, scrollToRecommended: (currentPosition: number) => void }) {
     const [hovering, hoveringSet] = useState(startHovering ?? false)
 
+    const pricingTableRef = useRef<HTMLDivElement>(null!)
+
+    useEffect(() => {
+        if (recommended && window.innerWidth < 800) {
+            scrollToRecommended(pricingTableRef.current.offsetLeft)
+        }
+    }, [])
+
     return (
-        <div className={`${styles.pricingTable} ${hovering && styles.pricingTableHovering}`} style={{}} onMouseEnter={() => { hoveringSet(true) }} onMouseLeave={() => { hoveringSet(false) }}>
+        <div ref={pricingTableRef} className={`${styles.pricingTable} ${hovering && styles.pricingTableHovering}`} style={{}} onMouseEnter={() => { hoveringSet(true) }} onMouseLeave={() => { hoveringSet(false) }}>
             <p style={{ textTransform: "capitalize", fontSize: "var(--mediumFontSize)" }}>{pricingTable.planName}</p>
 
             {recommended && (
                 <p style={{ position: "absolute", top: 0, right: 0, margin: "1rem", backgroundColor: "var(--primaryColor)", color: "#fff", padding: ".5rem", fontSize: "var(--smallFontSize)", borderRadius: "1rem", fontWeight: "bold" }}>Bestseller</p>
             )}
 
-            <p>{pricingTable.shortDescription}</p>
+            <p style={{ minHeight: "50px" }}>{pricingTable.shortDescription}</p>
 
             <div style={{ display: "flex", fontSize: "var(--largerFontSize)", alignItems: "center" }}>
                 {pricingTable.pricing >= 1000 && (
