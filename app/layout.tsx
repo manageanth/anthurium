4ec618246a14d3cdca2bd862f4b420a219f15dc2
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-05-21 08:53:11 -0500

    test 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,23 +9,24 @@ import Footer from "@/components/footer/Footer";
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://anthsolutions.com'),
   title: "Anth-Solutions - Expert Web Development & Mobile App Solutions | Next.js & React Native Specialists",
   description: "Anth-Solutions is a leading web development agency specializing in high-performance Next.js websites and seamless React Native mobile app solutions. Our expert team delivers tailored digital experiences, from custom e-commerce platforms to robust software solutions. Contact us to elevate your online presence",
-
   openGraph: {
     title: "Anth-Solutions - Expert Web Development & Mobile App Solutions",
-    description: "Anth-Solutions is a leading web development agency specializing in high-performance Next.js websites and seamless React Native mobile app solutions.",
-    // images: [require(`@/public/logo.svg`).default.src]
-    images: [{
-      width: 1800,
-      // url: require(`@/public/logo.svg`).default.src, // Must be an absolute URL
-      url: "https://images.pexels.com/photos/270637/pexels-photo-270637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", // Must be an absolute URL
-      height: 1600,
-      alt: 'logo',
-    }],
-
+    description: "Anth-Solutions is a leading web development agency specializing in high-performance Next.js websites and seamless React Native mobile app solutions."    
   }
 };
+
+    // images: [require(`@/public/logo.svg`).default.src]
+    // images: [{
+      
+    //   // url: require(`@/public/logo.svg`).default.src, // Must be an absolute URL
+    //   url: require(`@/public/logo.svg`).default.src, // Must be an absolute URL
+    //   width: 1800,
+    //   height: 1600,
+    //   alt: 'logo',
+    // }],
 
 
 export default function RootLayout({
